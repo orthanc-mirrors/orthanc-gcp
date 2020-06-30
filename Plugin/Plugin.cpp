2e96e91363677cfdc93139ae7f3ae94d281ddd31
Sebastien Jodogne <s.jodogne@gmail.com>
2020-06-30 09:29:50 +0200

    simplification

--- Plugin/Plugin.cpp
+++ Plugin/Plugin.cpp
@@ -131,12 +131,8 @@ extern "C"
   {
     OrthancPlugins::SetGlobalContext(context);
 
-#if defined(ORTHANC_FRAMEWORK_VERSION_IS_ABOVE)
-#  if ORTHANC_FRAMEWORK_VERSION_IS_ABOVE(1, 7, 2)
+#if defined(ORTHANC_FRAMEWORK_VERSION_IS_ABOVE)  // This indicates Orthanc framework >= 1.7.2
     Orthanc::Logging::InitializePluginContext(context);
-#  else
-    Orthanc::Logging::Initialize(context);
-#  endif
 #else
     Orthanc::Logging::Initialize(context);
 #endif
